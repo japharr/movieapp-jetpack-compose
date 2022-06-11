46a716bf49b72493f19366d2b40609f3bd513c3e
Jelil Adesina <jtutoff@gmail.com>
2022-06-12 00:43:29 +0100

    navigating to detail page

---
+++ app/src/main/java/com/japharr/movieapp/navigation/MovieScreens.kt
@@ -0,0 +1,18 @@
+package com.japharr.movieapp.navigation
+
+import java.lang.IllegalArgumentException
+
+enum class MovieScreens {
+    HomeScreen,
+    DetailsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): MovieScreens =
+            when(route?.substringBefore("/")) {
+                HomeScreen.name -> HomeScreen
+                DetailsScreen.name -> DetailsScreen
+                null -> HomeScreen
+                else -> throw IllegalArgumentException("Route $route not found!")
+            }
+    }
+}
---
+++ app/src/main/java/com/japharr/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,71 @@
+package com.japharr.movieapp.screens.home
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.japharr.movieapp.navigation.MovieScreens
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
+            Text(text = "Movies")
+        }
+    },) {
+        MainContent(navController)
+    }
+}
+
+@Composable
+fun MainContent(navController: NavController, movieList: List<String> = listOf("Avatar", "Harry Porter", "300",
+    "Beauty and The Beast", "Father Stuat", "Spy Kids", "Spider Man", "Iron Man", "Rogue")) {
+    Column(modifier = Modifier.padding(11.dp)) {
+        LazyColumn() {
+            items(items = movieList) {
+                MovieRow(it) { movie ->
+                    Log.d("Tag", "$movie is clicked")
+                    navController.navigate(MovieScreens.DetailsScreen.name)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClicked: (String) -> Unit) {
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp)
+        .clickable {
+            onItemClicked(movie)
+        },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                shape = RectangleShape, elevation = 4.dp) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
+    }
+}
